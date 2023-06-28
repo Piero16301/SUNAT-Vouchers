675965fc64022f70e7323441c71d663b7df22195
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2023-06-28 11:15:25 -0500

    Inicio proyecto

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.sunat_vouchers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:sunat_vouchers/app/app.dart';
+import 'package:sunat_vouchers/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
